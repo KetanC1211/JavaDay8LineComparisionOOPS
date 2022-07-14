cc55b5d95656b3bb420d1f4eeb8003d0aeef8af2
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-14 17:47:11 +0530

    Refactor UC-1

--- src/com/bridgelabz/Line_Comparison.java
+++ src/com/bridgelabz/Line_Comparison.java
@@ -5,7 +5,15 @@ public class Line_Comparison {
 		int z = ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
 		double length = Math.sqrt(z);
 		return length;
-	}	
+	}
+	public static void lengthCompare(double length1, double length2){
+		if(length1==length2) {
+			System.out.println("length of lines are equal");
+		}
+		else {
+			System.out.println("length of lines are not equal");
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,7 +22,14 @@ public class Line_Comparison {
 		int y1 = sc.nextInt();
 		int x2 = sc.nextInt();
 		int y2 = sc.nextInt();
-		double length = Line_Comparison.lineLength(x1, x2, y1, y2);
-		System.out.println("Length of line is "+length);
+		double length1 = Line_Comparison.lineLength(x1, x2, y1, y2);
+		System.out.println("Length of line is "+length1);
+		int a1 = sc.nextInt();
+		int b1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int b2 = sc.nextInt();
+		double length2 = Line_Comparison.lineLength(a1, a2, b1, b2);
+		System.out.println("Length of line is "+length2);
+		Line_Comparison.lengthCompare(length1,length2);
 	}
 }
